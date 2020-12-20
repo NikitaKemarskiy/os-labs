1e863b47051f4d746f8bc94aaf47c87900229be9
Nikita Kemarskiy <nikitakemarskiy@gmail.com>
2020-12-20 20:14:04 +0200

    Lab3 README.md was updated.

--- lab3/src/com/nikita/Dispatcher/Dispatcher.java
+++ lab3/src/com/nikita/Dispatcher/Dispatcher.java
@@ -3,11 +3,15 @@ package com.nikita.Dispatcher;
 import com.nikita.Job.Job;
 import com.nikita.Queue.QueuePriorityMap;
 
+import java.util.Date;
+
 public class Dispatcher implements Runnable {
     private QueuePriorityMap<Job> queuePriorityMap;
     private int executedJobs = 0;
     private int totalWaitingTime = 0;
+    private int totalIdleTime = 0;
     private int totalJobs;
+    private Date startedAt = new Date();
 
     public Dispatcher(
         QueuePriorityMap<Job> queuePriorityMap,
@@ -26,7 +30,9 @@ public class Dispatcher implements Runnable {
                 synchronized (queuePriorityMap) {
                     job = queuePriorityMap.pop();
                     if (job == null) {
+                        long timestamp = new Date().getTime();
                         queuePriorityMap.wait();
+                        totalIdleTime += new Date().getTime() - timestamp;
                         continue;
                     }
                 }
@@ -40,6 +46,8 @@ public class Dispatcher implements Runnable {
                     System.out.println("===================");
                     System.out.printf("- Jobs executed: %d%n", executedJobs);
                     System.out.printf("- Average waiting time: %d%n", totalWaitingTime / executedJobs);
+                    System.out.printf("- Idle time: %d%n", totalIdleTime);
+                    System.out.printf("- Idle time percentage: %.2f%%%n", ((double) totalIdleTime / (new Date().getTime() - startedAt.getTime())) * 100);
                     System.exit(0);
                 }
             } catch (Exception err) {
