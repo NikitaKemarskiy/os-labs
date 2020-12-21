1b589baa0a9db13e5c357fced3db39816d7cfaa3
Nikita Kemarskiy <nikitakemarskiy@gmail.com>
2020-12-21 14:03:25 +0200

    Lab6 was added.

--- lab6/src/com/nikita/Main.java
+++ lab6/src/com/nikita/Main.java
@@ -8,21 +8,17 @@ public class Main {
         int size = 200;
 
         long initialExecutionTime = initial(size);
-        long improvedExecutionTime = improved(size);
 
         System.out.printf("- Initial code execution time: %d%n", initialExecutionTime);
-        System.out.printf("- Improved code execution time: %d%n", improvedExecutionTime);
-        System.out.printf("- Comparative: %.2f%%%n", (double) initialExecutionTime / improvedExecutionTime * 100);
     }
 
     public static long initial(int size) {
         long startTimestamp = new Date().getTime();
 
-        int[][][] arr = new int[size][size][size];
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                for (int k = 0; k < size; k++) {
-                    arr[k][j][i]++;
+        for (int i = 0; i < 10; i++) {
+            for (int j = (int) 1e6; j > 0; j--) {
+                if (func1() || func2(i)) {
+                    System.out.println(">>> Inside if [" + i + "][" + j + "]");
                 }
             }
         }
@@ -43,5 +39,31 @@ public class Main {
         }
 
         return new Date().getTime() - startTimestamp;
+    }
+
+    public static boolean func1() {
+        try {
+            for (int i = 0; i < 100; i++) {
+                Thread.sleep(100);
+            }
+        } catch (InterruptedException err) {
+            System.err.println(err);
+            System.exit(1);
+        }
+        return true;
+    }
+    public static boolean func2(int num) {
+        try {
+            for (int i = 0; i < 10; i++) {
+                Thread.sleep(1);
+                if (i > num) {
+                    break;
+                }
+            }
+        } catch (InterruptedException err) {
+            System.err.println(err);
+            System.exit(1);
+        }
+        return true;
     }
 }
